e3b71ea85030b11cacc562f3d492282e18cf89f3
Enami Yui <77408411+EnamiYui@users.noreply.github.com>
2021-02-28 22:59:41 +0900

    abc193

---
+++ abc193/a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double A,B;
+    cin >> A >> B;
+    double C = A - B;
+    double D = C / A;
+    double E = (D * 100.0);
+    cout << E << endl;
+}
